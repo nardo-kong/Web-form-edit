1600cc4f35f3cda0cc4803c1453d4a97486c45a1
nardo-kong <114288961+nardo-kong@users.noreply.github.com>
2024-05-04 16:21:05 +0800

    Delete src directory

--- src/main/java/com/example/demo/Repositories/JpaRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.Repositories;
-
-public interface JpaRepository<T1, T2> {
-
-}
--- src/main/java/com/example/demo/Repositories/OptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repositories;
-import com.example.demo.Entities.Option;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionRepository extends JpaRepository<Option, Long> {
-}
