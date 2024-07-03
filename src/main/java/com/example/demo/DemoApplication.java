c01f28e11ec53ec0162b41adbe90e575cdcf0baa
Nardo <nardokong@gmail.com>
2024-07-03 17:05:05 +0800

    variable test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // 获取环境变量SPRING_DATASOURCE_PASSWORD的值
+        String datasourcePassword = System.getenv("SPRING_DATASOURCE_PASSWORD");
+        
+        // 打印环境变量的值，如果未设置则打印null
+        System.out.println("SPRING_DATASOURCE_PASSWORD: " + datasourcePassword);
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
